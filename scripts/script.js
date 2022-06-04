385de4ed2eea7efa559444b4ad13aa06721f85c0
mr7kitkat <mr7kitkat@gmail.com>
2022-06-05 00:11:53 +0530

    add a basic structure, generate maze and some little functionality with js and put some style

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+// Generate maze using a number
+function generateMaze(parentElem, size) {
+    const mazeAry = []
+    parentElem.innerHTML = '';
+
+    parentElem.style.cssText = `        
+    grid-template-columns: repeat(${size}, 1fr);
+    grid-template-rows: repeat(${size}, 1fr);
+    `
+
+    for(let i = 0; i < size; i++) {
+        const arr = [];
+        for(let j = 0; j < size; j++) {
+            parentElem.innerHTML += `<img src="" alt="" class="cell" data-row="${i}" data-col="${j}">`;
+            arr.push(null);
+        }
+        mazeAry.push(arr);
+    }
+
+    return mazeAry;
+}
+
+const main = document.querySelector("main");
+const mazeAry = generateMaze(main, 3);
+
+// on click event add icon to svg
+const cells = document.querySelectorAll(".cell");
+let currentMove = './images/cross.png';
+
+cells.forEach(cell => {
+    cell.addEventListener("click", (e) => {
+        e.target.setAttribute('src', currentMove);
+        const row = Number(e.target.dataset.row);
+        const col = Number(e.target.dataset.col);
+        if (currentMove.includes("cross")) {
+            mazeAry[row][col] = 'x';
+            currentMove = './images/circle.png';
+        }else {
+            mazeAry[row][col] = 'o';
+            currentMove = './images/cross.png' ;
+        }
+
+        console.table(mazeAry);
+    })
+})
+
+
+
+
+
