0816cad1014e4ece5406e8fb026eee2534fddffc
mr7kitkat <mr7kitkat@gmail.com>
2022-06-06 00:25:40 +0530

    Overall app is working but is just 1.0 so alot of refactoring is needed

--- scripts/script.js
+++ scripts/script.js
@@ -21,17 +21,20 @@ function generateMaze(parentElem, size) {
 }
 
 const main = document.querySelector("main");
+const currMove = document.querySelector("header img");
 const mazeAry = generateMaze(main, 3);
 
 // on click event add icon to svg
 const cells = document.querySelectorAll(".cell");
 let currentMove = './images/cross.png';
+currMove.setAttribute("src", currentMove);
 
 cells.forEach(cell => {
     cell.addEventListener("click", (e) => {
-
         const row = Number(e.target.dataset.row);
         const col = Number(e.target.dataset.col);
+
+
         if(!mazeAry[row][col]) {
             e.target.setAttribute('src', currentMove);
 
@@ -42,12 +45,35 @@ cells.forEach(cell => {
                 mazeAry[row][col] = 'o';
                 currentMove = './images/cross.png' ;
             }
-    
-            console.table(validateCol(mazeAry, row, col));    
+            
+            let winner = validation(mazeAry, row, col);
+            if(winner === 'x') {
+                currentMove = './images/cross.png' ;
+                currMove.nextElementSibling.innerText = 'Wins!'
+            }else if(winner === 'o') {
+                currentMove = './images/circle.png' ;
+                currMove.nextElementSibling.innerText = 'Wins!'
+            }
+
+            currMove.setAttribute("src", currentMove);
         }
 
     })
 })
+
+
+
+
+
+// Functions for Validation
+function validation(ary, row, col) {
+    const rowPart = validateRow(ary, row, col);
+    const colPart = validateCol(ary, row, col);
+    const ltrPart = ltrDiagonalMatch(ary, row, col);
+    const rtlPart = rtlDiagonalMatch(ary, row, col);
+
+    return rowPart || colPart || ltrPart || rtlPart || false;
+}
 
 
 
@@ -64,15 +90,41 @@ function validateRow(ary, row, col) {
 
 function validateCol(ary, row, col) {
     const clickedCell = ary[row][col];
-    let flag = false;
+    let flag = true;
     for (let i = 0; i < ary.length; i++) {
-        if (ary[i][col] === clickedCell) {
-            flag = true;
-        }else {
+        if (ary[i][col] !== clickedCell) {
             flag = false;
         }
     }
 
     return flag ? clickedCell : false;
+}
+
+
+function ltrDiagonalMatch (ary, row, col) {
+    let ltrDiagonalCheck = true;
+    const clickedCell = ary[row][col];
+
+    for (let i = 0; i < ary.length; i++) {
+        if(ary[i][i] !== clickedCell) {
+            ltrDiagonalCheck = false;
+        }
+    }
+
+    return ltrDiagonalCheck ? clickedCell : false;
+}
+
+function rtlDiagonalMatch (ary, row, col) {
+    let rtlDiagonalCheck = true;
+    const clickedCell = ary[row][col];
+
+    const maxidx = ary.length - 1;
+    for (let i = 0; i < ary.length; i++) {
+        if(ary[i][maxidx - i] !== clickedCell) {
+            rtlDiagonalCheck = false;
+        }
+    }
+
+    return rtlDiagonalCheck ? clickedCell : false;
 }
 
