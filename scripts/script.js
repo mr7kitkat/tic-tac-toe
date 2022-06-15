3eacd1f76d0726a86ad87312daf18f9a510682c6
mr7kitkat <mr7kitkat@gmail.com>
2022-06-15 06:49:33 +0530

    Application complete for P2P fight, need to add P2Computer implimentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,130 +1,148 @@
-// Generate maze using a number
-function generateMaze(parentElem, size) {
-    const mazeAry = []
-    parentElem.innerHTML = '';
+(function(){
+    let mod = {
+        lastClick: 'x',
+        winner: null,
+        init: function() {
+            this.cacheDOM();
+            this.generateMaze(this.maze, 3);
+            this.renderLastClick();
+        },
+        cacheDOM : function(){
+            this.maze = document.querySelector("main");
+            this.currMove = document.querySelector("header img");
+            this.winStatus = document.querySelector(".win-status");
+            this.winnerIMG = this.winStatus.querySelector(".winner-img");
+            this.replayBTN = this.winStatus.querySelector(".replay");
 
-    parentElem.style.cssText = `        
-    grid-template-columns: repeat(${size}, 1fr);
-    grid-template-rows: repeat(${size}, 1fr);
-    `
-
-    for(let i = 0; i < size; i++) {
-        const arr = [];
-        for(let j = 0; j < size; j++) {
-            parentElem.innerHTML += `<img src="" alt="" class="cell" data-row="${i}" data-col="${j}">`;
-            arr.push(null);
-        }
-        mazeAry.push(arr);
-    }
-
-    return mazeAry;
-}
-
-const main = document.querySelector("main");
-const currMove = document.querySelector("header img");
-const mazeAry = generateMaze(main, 3);
-
-// on click event add icon to svg
-const cells = document.querySelectorAll(".cell");
-let currentMove = './images/cross.png';
-currMove.setAttribute("src", currentMove);
-
-cells.forEach(cell => {
-    cell.addEventListener("click", (e) => {
-        const row = Number(e.target.dataset.row);
-        const col = Number(e.target.dataset.col);
-
-
-        if(!mazeAry[row][col]) {
-            e.target.setAttribute('src', currentMove);
-
-            if (currentMove.includes("cross")) {
-                mazeAry[row][col] = 'x';
-                currentMove = './images/circle.png';
-            }else {
-                mazeAry[row][col] = 'o';
-                currentMove = './images/cross.png' ;
+        },
+        getImg: function(lookupval) {
+            let src;
+            if(lookupval === 'x') {
+                src = '../images/cross.png'
+            }else if (lookupval === 'o') {
+                src = '../images/circle.png'
             }
-            
-            let winner = validation(mazeAry, row, col);
-            if(winner === 'x') {
-                currentMove = './images/cross.png' ;
-                currMove.nextElementSibling.innerText = 'Wins!'
-            }else if(winner === 'o') {
-                currentMove = './images/circle.png' ;
-                currMove.nextElementSibling.innerText = 'Wins!'
+            return src;
+        },
+        render: function(ary) {
+            for (let i = 0; i < ary.length; i++) {
+                for (let j = 0; j < ary[i].length; j++) {
+                    const cellElem = document.querySelector(`[data-cell="r${i}c${j}"]`)
+                    const cell = ary[i][j];
+                    let src = this.getImg(cell);
+                    if(src) {
+                        cellElem.setAttribute('src', src);
+                    }
+                }
+            }
+        },
+        renderLastClick: function() {
+            let src = this.getImg(this.lastClick);
+            this.currMove.setAttribute("src", src);
+        },
+        generateMaze: function(parentElem, size) {
+            this.mazeAry = [];
+            parentElem.innerHTML = '';
+
+            parentElem.style.cssText = `        
+            grid-template-columns: repeat(${size}, 1fr);
+            grid-template-rows: repeat(${size}, 1fr);
+            `
+
+            for(let i = 0; i < size; i++) {
+                const arr = [];
+                for(let j = 0; j < size; j++) {
+                    parentElem.innerHTML += `<img src="" alt="" class="cell" data-cell="r${i}c${j}">`;
+                    arr.push(null);
+                }
+                this.mazeAry.push(arr);
+            }
+            this.cells = document.querySelectorAll(".cell");
+            this.watchClicks()
+        },
+        watchClicks() {
+            this.cells.forEach(cell => {
+                cell.addEventListener("click", (e) => {
+                    if(!this.winner){
+                        const cellVal = e.target.dataset.cell.split("");
+                        let [, row, , col] = cellVal
+                        row = +row;
+                        col = +col;
+                        
+                        if(!this.mazeAry[row][col]) {
+                            if(this.lastClick === 'x') {
+                                this.mazeAry[row][col] = this.lastClick;
+                                this.lastClick = 'o'
+                            }
+                            else if(this.lastClick === 'o') {
+                                this.mazeAry[row][col] = this.lastClick;
+                                this.lastClick = 'x'
+                            }
+                        }
+                        this.winner = this.validation(this.mazeAry, row, col);
+                    }
+                    this.renderLastClick()
+                    this.render(this.mazeAry)
+                    this.showWin()
+
+                })
+            });
+        },
+        validation: function(ary, row, col) {
+            function checkCells() {
+                const maxidx = ary.length - 1;
+                const clickedCell = ary[row][col];
+                let valAry = Array(4).fill(true);
+                for (let i = 0; i < ary.length; i++) {
+                    // FOR ROW PART
+                    const r = ary[i];
+                    valAry[0] = r.every(item => item === clickedCell);
+                    if (valAry[0]) {
+                        break;
+                    }
+                    
+                    if (ary[i][col] !== clickedCell) {
+                        valAry[1] = false;
+                    }
+                    
+                    if(ary[i][i] !== clickedCell) {
+                        valAry[2] = false;
+                    }
+                    
+                    if(ary[i][maxidx - i] !== clickedCell) {
+                        valAry[3] = false;
+                    }
+                }
+                const check = valAry.some(i => i === true);
+                return check ? clickedCell : false;
             }
 
-            currMove.setAttribute("src", currentMove);
-        }
+            const ret = checkCells();
+            return ret;
+        },        
+        replay: function() {
+            console.log(this)
+            this.winner = null;
+            this.lastClick = 'x';
+            this.generateMaze(this.maze, 3);
+            this.winnerIMG.setAttribute("src", '');
+            this.winStatus.classList.remove("show");
+        },
+        showWin: function() {
+            if (this.winner) {
+                setTimeout(() => {
+                    const img = this.getImg(this.winner);
+                    this.winnerIMG.setAttribute("src", img);
+                    this.winStatus.classList.add("show");
+                },500)    
+            }
 
-    })
-})
-
-
-
-
-
-// Functions for Validation
-function validation(ary, row, col) {
-    const rowPart = validateRow(ary, row, col);
-    const colPart = validateCol(ary, row, col);
-    const ltrPart = ltrDiagonalMatch(ary, row, col);
-    const rtlPart = rtlDiagonalMatch(ary, row, col);
-
-    return rowPart || colPart || ltrPart || rtlPart || false;
-}
-
-
-
-function validateRow(ary, row, col) {
-    const clickedCell = ary[row][col];
-    const check = ary[row].every(item => {
-        return item === clickedCell;
-    });
-
-    return check ? clickedCell : false;
-}
-
-
-
-function validateCol(ary, row, col) {
-    const clickedCell = ary[row][col];
-    let flag = true;
-    for (let i = 0; i < ary.length; i++) {
-        if (ary[i][col] !== clickedCell) {
-            flag = false;
+            this.replayBTN.addEventListener("click", () => {
+                this.replay()
+            })
         }
     }
 
-    return flag ? clickedCell : false;
-}
-
-
-function ltrDiagonalMatch (ary, row, col) {
-    let ltrDiagonalCheck = true;
-    const clickedCell = ary[row][col];
-
-    for (let i = 0; i < ary.length; i++) {
-        if(ary[i][i] !== clickedCell) {
-            ltrDiagonalCheck = false;
-        }
-    }
-
-    return ltrDiagonalCheck ? clickedCell : false;
-}
-
-function rtlDiagonalMatch (ary, row, col) {
-    let rtlDiagonalCheck = true;
-    const clickedCell = ary[row][col];
-
-    const maxidx = ary.length - 1;
-    for (let i = 0; i < ary.length; i++) {
-        if(ary[i][maxidx - i] !== clickedCell) {
-            rtlDiagonalCheck = false;
-        }
-    }
-
-    return rtlDiagonalCheck ? clickedCell : false;
-}
+    mod.init();
+})();
 
