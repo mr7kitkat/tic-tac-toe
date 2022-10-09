0aeb6575b8141cb343d9504ba40a4684fd20c47e
Aman <mr7kitkat@gmail.com>
2022-10-09 10:25:23 +0530

    Added rendering for the maze

--- scripts/script.js
+++ scripts/script.js
@@ -6,67 +6,91 @@
 5. If maximum limit reached than show Draw and 
 */
 
-function createMaze(num) {
-  if (num < 3) {
-    return;
-  }
-  return Array(num).fill(Array(num).fill(null));
-}
-
-// 1. Start with X or O
-// 2. Change one move after each input
-function gameInput() {
-  let input = "x";
-
-  return {
-    currentInput() {
-      return input;
-    },
-    nextInput() {
-      return input === "x" ? "o" : "x";
-    },
-    changeInput() {
-      input = this.nextInput();
-    },
-  };
-}
-
-// 3. on Each input check for winning from that cell
-function checkValidation(ary, input, row, col) {
-  function validateRow(ary, row, input) {
-    return ary[row].every((item) => item === input);
-  }
-
-  function validateCol(ary, col, input) {
-    return ary.every((item) => item[col] === input);
-  }
-
-  function getDiagonalAxis(ary) {
-    const diagonalAry = {
-      left: [],
-      right: [],
-    };
-
-    let len = ary.length - 1;
-    for (let row = 0; row <= len; row++) {
-      diagonalAry.left.push(ary[row][row]);
-      diagonalAry.right.push(ary[row][len - row]);
-    }
-
-    return diagonalAry;
-  }
-
-  function validateCrossSides(ary, input) {
-    if (ary.length % 2 === 0) {
+(function () {
+  // MODEL ------------------------------------------------------------------------
+  function createMaze(num) {
+    if (num < 3) {
       return;
     }
-    const diagonalAxis = getDiagonalAxis(ary);
-    console.log(diagonalAxis);
-
-    const left = diagonalAxis["left"].every((item) => item === input);
-    const right = diagonalAxis["right"].every((item) => item === input);
-    return left || right;
+    return Array(num).fill(Array(num).fill(null));
   }
-}
 
-console.log(validateCrossSides(arr, 0));
+  // 1. Start with X or O
+  // 2. Change one move after each input
+  function gameInput() {
+    let input = "x";
+
+    return {
+      currentInput() {
+        return input;
+      },
+      nextInput() {
+        return input === "x" ? "o" : "x";
+      },
+      changeInput() {
+        input = this.nextInput();
+      },
+    };
+  }
+
+  // 3. on Each input check for winning from that cell
+  function checkValidation(ary, input, row, col) {
+    function validateRow(ary, row, input) {
+      return ary[row].every((item) => item === input);
+    }
+
+    function validateCol(ary, col, input) {
+      return ary.every((item) => item[col] === input);
+    }
+
+    function getDiagonalAxis(ary) {
+      const left = [];
+      const right = [];
+
+      let len = ary.length - 1;
+      for (let row = 0; row <= len; row++) {
+        left.push(ary[row][row]);
+        right.push(ary[row][len - row]);
+      }
+
+      return [left, right];
+    }
+
+    function validateCrossSides(ary, input) {
+      if (ary.length % 2 === 0) {
+        return;
+      }
+      const diagonalAxis = getDiagonalAxis(ary);
+      return diagonalAxis.some((subAry) =>
+        subAry.every((item) => item === input)
+      );
+    }
+
+    return (
+      validateRow(ary, row, input) ||
+      validateCol(ary, col, input) ||
+      validateCrossSides(ary, input) ||
+      false
+    );
+  }
+
+  // VIEW ------------------------------------------------
+  function generateMaze(ary, parentElement) {
+    let innerHtml = "";
+
+    ary.forEach((row, ridx) => {
+      row.forEach((col, cidx) => {
+        innerHtml += `
+        <div class="cell border-for-maze flex justify-center align-center" data-rc="${ridx},${cidx}">
+          <img src="" alt="">
+        </div>
+        `;
+      });
+    });
+
+    parentElement.innerHTML = innerHtml;
+    parentElement.style.gridTemplateColumns = `repeat(${ary.length},1fr)`;
+    parentElement.style.gridTemplateRows = `repeat(${ary.length},1fr)`;
+  }
+  
+})();
